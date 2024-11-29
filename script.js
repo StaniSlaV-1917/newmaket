e9dc2b263901c5c611b548557e4a3af192bd2e2e
StaniSlaV-1917 <165366543+StaniSlaV-1917@users.noreply.github.com>
2024-11-29 13:43:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let menu = document.querySelector(".menu");
+    let menuToggle = document.querySelector(".head4");
+  
+    menuToggle.addEventListener("click", function(e) {
+      e.preventDefault();
+      menu.classList.toggle("show-menu");
+      this.classList.toggle("active");
+    });
+  
+    window.addEventListener('resize', function() {
+      if (this.window.innerWidth > 375 && menu.classList.contains('show-menu')) {
+        menu.classList.remove('show-menu');
+        menuToggle.classList.remove("active");
+      }
+    });
+  });
+//--------------------------------------------------------------------------
+  document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.chel');
+    let currentSlide = 0;
+
+
+    function updateSlider() {
+        slides.forEach(slide => slide.classList.remove('active'));
+        slides[currentSlide].classList.add('active');
+    }
+
+
+    setInterval(function() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        updateSlider();
+    }, 3000); 
+
+    updateSlider();
+});
